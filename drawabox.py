b0458b2cb7be035a9e126870ca24e093000b14c6
Blake Golliher <blakegolliher@gmail.com>
2013-12-28 00:26:01 -0800

    Create drawabox.py

---
+++ drawabox.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("width", type=int,
+                    help="The width of the box.")
+parser.add_argument("height", type=int,
+                    help="This will be the height of the box.")
+args = parser.parse_args()
+width = args.width
+height = args.height
+
+print "*" * width
+for middle in range(height - 2):
+        middle = "|" + ' ' * (width - 2) + "|"
+        print middle
+print "*" * width
