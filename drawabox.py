9a271d5b37018868715df9e999ed736010e29f12
Blake Golliher <blakegolliher@gmail.com>
2016-08-25 14:41:56 -0700

    Update drawabox.py

--- drawabox.py
+++ drawabox.py
@@ -3,16 +3,17 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("width", type=int,
-                    help="The width of the box.")
-parser.add_argument("height", type=int,
-                    help="This will be the height of the box.")
+parser.add_argument("width", type=int, help="The width of the box.")
+parser.add_argument("height", type=int, help="This will be the height of the box.")
 args = parser.parse_args()
+
 width = args.width
 height = args.height
 
-print "*" * width
-for middle in range(height - 2):
-        middle = "|" + ' ' * (width - 2) + "|"
-        print middle
-print "*" * width
+def box(width,height):
+    print "*" * width
+    for middle in range(height - 2):
+        print "|" + ' ' * (width - 2) + "|"
+    print "*" * width
+
+box(width,height)
